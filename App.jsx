6347640f09e8bd9cdcd6d4134b1a32bc4ce516da
David Darnes <me@daviddarnes.com>
2024-07-26 16:32:29 +0100

    Initial app code

---
+++ App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Avatar } from "@zh-engineer/airorange-react";
+
+export default function App() {
+  return (
+    <div>
+      <div className="container">
+        <div className="row">
+          <Avatar
+            name="David Darnes"
+            src="https://hub.darn.es/content/images/size/w4000/2022/02/dave-penfold.jpg"
+          />
+          <Avatar
+            name="Seth Corker"
+            src="https://sethcorker.com/static/web_prof-small-5ea7bafb5e58043f7536b63497995d29.jpg"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
